cf880726bfe84ed5ee6d8326a01ba0be1ac03293
Dylan Borchert <dborc610@mtroyal.ca>
2023-09-22 07:55:43 -0600

    .

--- App/camru-app/vite.config.ts
+++ App/camru-app/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin(), pluginRewriteAll()],
 	server: {
 		port: 3000,
 		host: "0.0.0.0",
